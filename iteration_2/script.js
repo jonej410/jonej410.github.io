e8fd8d60af03f0eae05f40371582eb01a603ef05
jonej410 <javariusjones@Javariuss-MacBook-Pro.local>
2016-12-17 17:59:06 -0500

    I2 – Custom Function
    Added a custom function to Iteration 2

--- iteration_2/script.js
+++ iteration_2/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() { //always do this first. A special event
 
-//  random values between 0 to 100, to rx and ry
+
+//here's the function
+
+drawRainDrops();
+
+//random values between 0 to 100, to rx and ry
 
 var rx = Math.floor(Math.random()*100);
 var ry = Math.floor(Math.random()*100);
@@ -23,20 +28,23 @@ var ry = Math.floor(Math.random()*100);
 
   console.log("rx: " + rx + " ry: " + ry);
 
-//first set of raindrops
-  for (j = 0; j < 100; j++) {
-    for (i = 0; i < 20; i++) {
-      $("body").append( "<div class='rain' style='left:" + i*5 + "vw; top:" + j*50 + "px;'></div>" );
-      console.log("element " + j + ", " + i + " added");
+function drawRainDrops () {
+  //first set of raindrops
+    for (j = 0; j < 100; j++) {
+      for (i = 0; i < 20; i++) {
+        $("body").append( "<div class='rain' style='left:" + i*5 + "vw; top:" + j*50 + "px;'></div>" );
+        console.log("element " + j + ", " + i + " added");
+      }
     }
-  }
 
-//second set of raindrops
-  for (k = 0; k < 100; k++) {
-    for (l = 0; l < 30; l++) {
-      $("body").append( "<div class='second' style='left:" + l*9 + "vw; top:" + k*56 + "px;'></div>" );
-      console.log("element " + k + ", " + l + " added");
+  //second set of raindrops
+    for (k = 0; k < 100; k++) {
+      for (l = 0; l < 30; l++) {
+        $("body").append( "<div class='second' style='left:" + l*9 + "vw; top:" + k*56 + "px;'></div>" );
+        console.log("element " + k + ", " + l + " added");
+      }
     }
-  }
+}
+
 
   });
