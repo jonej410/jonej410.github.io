13ac15816628daa656cc2dbd8c6e23b5e66d05ce
Javarius JonesE <jonej410@newschool.eduS>
2017-07-14 20:49:09 -0400

    cam

---
+++ website/assets/js/home.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+//   $(".s-container").draggable();
+// });
+
+
+
+var video = document.querySelector("#videoElement");
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+
+function videoError(e) {
+    // do something
+}
